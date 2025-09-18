121678153e166f334a5e7eea46c96ede752fcd81
sabrinagimenez98 <sabrinita9898@gmail.com>
2025-09-18 09:58:24 -0300

    Update Caso_7.py

--- Caso_7.py
+++ Caso_7.py
@@ -19,28 +19,41 @@ while opcion!=9:
     print("8.Ingresar asistencia")
     print("9.Salir del programa")
     opcion=int(input("\n Ingrese una opcion: "))
+
     if opcion==1:
         estudiantes.clear()
         asistencias.clear()
         cantidad= int(input("\n Cantidad de alumnos a ingresar: "))
         for i in range(cantidad):
-            estudiante=input(f"Ingrese nombre del Estudiante {i+1}: ").lower
+            estudiante=input(f"\nIngrese nombre del Estudiante {i+1}: ").title()
             while estudiante == "" or estudiante in estudiantes :
-                estudiante=input("Estudiante duplicado o invalido. Ingrese nuevamente: ")
+                estudiante=input("\n Estudiante duplicado o invalido. Ingrese nuevamente: ")
             asistencia=0
             estudiantes.append(estudiante)
             asistencias.append(asistencia)
             
     if opcion==2:
         for i in range(cantidad):
-            asistencia=random.randint(1,10)
-            asistencias.append(asistencia)
+            asistencias[i]=random.randint(1,10)
+        print(asistencias)
 
     if opcion==3:
             print("\n Listado de estudiantes con sus asistencias")
             for i in range(len(estudiantes)):
                  print(f"Estudiante {i+1}: {estudiantes[i]} - Asistencias: {asistencias[i]}")
 
-
-
+    if opcion==4:
+         estudiante=input("\n Ingrese estudiante que quiere consultar: ").title()
+         if estudiante in estudiantes:
+              indice=estudiantes.index(estudiante)
+              print(f"\n Estudiante: {estudiante} - Asistencia: {asistencias[indice]}")
+         
+    if opcion==5:
+        if 0 in asistencias:
+            for i in range(len(estudiantes)):
+                if asistencias[i]==0:
+                    print(estudiantes[i])
+        else:
+             print("\n No hay estudiantes con 0 asistecias")
+         
 print("\n Gracias por usar el sistema de Asistencias")
